9c4f3c66e090cbbb969e62c25d8d761518b93d0a
Davidson <Katie.Davidson@dfo-mpo.gc.ca>
2023-04-17 15:11:01 -0700

    update packages

--- nitinat/nitinat_esc.R
+++ nitinat/nitinat_esc.R
@@ -10,7 +10,9 @@ library(askpass)      # for functions within getNuSEDS()
 library(imputeTS)
 library(scales)       # for trans_new() in stat_smooth()
 library(splines)      # for quasipoisson in stat_smooth()
-library(cowplot)
+library(cowplot)      # for plot_grid()
+library(here)
+library(lubridate)
 
 # Set working directory -------------------------
 setwd("~/ANALYSIS/data")
@@ -20,6 +22,12 @@ options(scipen = 99999)
 a21_22esc.raw <- getNuSEDS("~/ANALYSIS/data/queryDocsMisc/nuseds_esc_query_Area21-22.json", password = "babysharkd0d0!")
 a22ages.raw <- getNuSEDS("~/ANALYSIS/data/queryDocsMisc/nuseds_padsCU_query_Area22.json", password = "babysharkd0d0!")
 nitTime.raw <- read_excel("run timing tables (updated).xlsx", sheet="nitinatR")
+nitWater <- read_excel("NitinatWaterLevelHydrometDataNov-25-2022 12-41.xlsx",sheet="Exported Data") %>%
+  mutate_at(c("Staff Gauge(mH20)", "Sensor Depth(mH20)"), as.numeric) %>%
+  mutate(date = as.Date(Time)) 
+nitsum2022 <- read_excel("SIL summary 2022.xlsx", sheet="NIT") %>%
+  mutate(Date = as.Date(Date)) 
+
 
 # Clean data -------------------------
 a21_22esc <- a21_22esc.raw %>% 
@@ -162,6 +170,191 @@ ggplot() +
   theme(plot.title = element_text(hjust=0.005, vjust=-7)),
 
 nrow=3)
+
+
+
+#################################################################################################################################################################
+
+
+#                                                           ENVIRONMENTAL + ESC DATA 2022
+
+
+# CHINOOK ---------------------------
+plot_grid(
+ggplot(data=nitWater %>% 
+         mutate(doy = lubridate::yday(date)) %>%
+         group_by(date) %>%
+         summarize(mean=mean(`Staff Gauge(mH20)`,na.rm=T), sd=sd(`Staff Gauge(mH20)`,na.rm=T),
+                   min=min(`Staff Gauge(mH20)`,na.rm=T), max=max(`Staff Gauge(mH20)`,na.rm=T))) +
+  geom_ribbon(aes(x=as.Date(date), ymin=min, ymax=max), fill="dodger blue", alpha=0.2) +
+  geom_line(aes(x=as.Date(date), y=mean), size=1, colour="dodger blue",alpha=0.7) +  
+  labs(y="Water gauge level (m)") +
+  scale_y_continuous(breaks=seq(1,4,by=0.5)) +
+  scale_x_date(date_breaks="1 day", date_labels="%b %d", limits=c(as.Date("2022-08-31"), as.Date("2022-11-02"))) +
+  theme_bw() +
+  theme(axis.text.x = element_text(angle=45, hjust=1),
+        axis.title.x = element_blank(),
+        axis.title = element_text(face="bold"),
+        axis.text = element_text(colour="black"),
+        panel.grid.minor = element_blank()),
+
+ggplot() +
+  geom_bar(data=nitsum2022 %>% 
+             filter(is.na(Comments)) %>%
+             group_by(Date, Species) %>%
+            summarize(holding=sum(`# Holding`,na.rm=T), spawning=sum(`# Spawning`,na.rm=T), total=sum(Total,na.rm=T)) %>% 
+             pivot_longer(cols=holding:total, names_to = "behaviour", values_to = "n") %>%
+             filter(behaviour!="total", Species=="Chinook"),
+           aes(x=as.Date(Date), y=n, fill=behaviour), 
+           stat="identity", width=1) +
+  scale_y_continuous(breaks=seq(0,6000,by=1000)) +
+  scale_x_date(date_breaks="1 day", date_labels="%b %d") +
+  labs(y="Raw # Chinook") +
+  theme_bw() +
+  theme(axis.text.x = element_blank(),
+        axis.title.x = element_blank(),
+        axis.title = element_text(face="bold"),
+        axis.text = element_text(colour="black"),
+        legend.position = c(0.1,0.7),
+        legend.title = element_text(face="bold"),
+        legend.background = element_rect(colour="black"),
+        panel.grid.minor = element_blank()),
+
+ggplot() +
+  geom_bar(data=nitsum2022  %>% 
+             filter(is.na(Comments)) %>%
+             group_by(Date, Species) %>%
+             summarize(new=mean(`%New`,na.rm=T)+0.01) %>% 
+             filter(Species=="Chinook"),
+           aes(x=as.Date(Date), y=new), 
+           stat="identity", width=1, fill="green") +
+  scale_x_date(date_breaks="1 day", date_labels="%b %d") +
+  labs(y="% New chinook") +
+  theme_bw() +
+  theme(axis.text.x = element_blank(),
+        axis.title.x = element_blank(),
+        axis.title = element_text(face="bold"),
+        axis.text = element_text(colour="black"),
+        legend.background = element_rect(colour="black"),
+        panel.grid.minor = element_blank()),
+
+ggplot() +
+  geom_bar(data=nitsum2022  %>% 
+             filter(is.na(Comments), Species=="Chinook") %>%
+             group_by(Date) %>%
+             summarize(dead=sum(Dead,na.rm=T),Total=sum(Total,na.rm=T)) %>% 
+             mutate(ratioDL = ifelse(dead>0,dead/Total,0.0001)),
+           aes(x=as.Date(Date), y=ratioDL), 
+           stat="identity", width=1, fill="maroon") +
+  scale_x_date(date_breaks="1 day", date_labels="%b %d") +
+  labs(y="Ratio of live:dead Chinook") +
+  theme_bw() +
+  theme(axis.text.x = element_text(angle=45, hjust=1),
+        axis.title.x = element_blank(),
+        axis.title = element_text(face="bold"),
+        axis.text = element_text(colour="black"),
+        legend.background = element_rect(colour="black"),
+        panel.grid.minor = element_blank()),
+
+nrow=4
+)
+#
+
+
+
+
+
+
+# CHUM ---------------------------
+plot_grid(
+  ggplot(data=nitWater %>% 
+           mutate(doy = lubridate::yday(date)) %>%
+           group_by(date) %>%
+           summarize(mean=mean(`Staff Gauge(mH20)`,na.rm=T), sd=sd(`Staff Gauge(mH20)`,na.rm=T),
+                     min=min(`Staff Gauge(mH20)`,na.rm=T), max=max(`Staff Gauge(mH20)`,na.rm=T))) +
+    geom_ribbon(aes(x=as.Date(date), ymin=min, ymax=max), fill="dodger blue", alpha=0.2) +
+    geom_line(aes(x=as.Date(date), y=mean), size=1, colour="dodger blue",alpha=0.7) +  
+    labs(y="Water gauge level (m)") +
+    scale_y_continuous(breaks=seq(1,4,by=0.5)) +
+    scale_x_date(date_breaks="1 day", date_labels="%b %d", limits=c(as.Date("2022-08-31"), as.Date("2022-11-02"))) +
+    theme_bw() +
+    theme(axis.text.x = element_text(angle=45, hjust=1),
+          axis.title.x = element_blank(),
+          axis.title = element_text(face="bold"),
+          axis.text = element_text(colour="black"),
+          panel.grid.minor = element_blank()),
+  
+  ggplot() +
+    geom_bar(data=nitsum2022 %>% 
+               filter(is.na(Comments)) %>%
+               group_by(Date, Species) %>%
+               summarize(holding=sum(`# Holding`,na.rm=T), spawning=sum(`# Spawning`,na.rm=T), total=sum(Total,na.rm=T)) %>% 
+               pivot_longer(cols=holding:total, names_to = "behaviour", values_to = "n") %>%
+               filter(behaviour!="total", Species=="Chum"),
+             aes(x=Date, y=n, fill=behaviour), 
+             stat="identity", width=1) +
+    scale_x_date(date_breaks="1 day", date_labels="%b %d") +
+    labs(y="Raw # Chum") +
+    theme_bw() +
+    theme(axis.text.x = element_blank(),
+          axis.title.x = element_blank(),
+          axis.title = element_text(face="bold"),
+          axis.text = element_text(colour="black"),
+          legend.position = c(0.1,0.7),
+          legend.title = element_text(face="bold"),
+          legend.background = element_rect(colour="black"),
+          panel.grid.minor = element_blank()),
+  
+  ggplot() +
+    geom_bar(data=nitsum2022  %>% 
+               filter(is.na(Comments)) %>%
+               group_by(Date, Species) %>%
+               summarize(new=mean(`%New`,na.rm=T)+0.01) %>% 
+               filter(Species=="Chum"),
+             aes(x=Date, y=new), 
+             stat="identity", width=1, fill="green") +
+    scale_x_date(date_breaks="1 day", date_labels="%b %d") +
+    labs(y="% New Chum") +
+    theme_bw() +
+    theme(axis.text.x = element_blank(),
+          axis.title.x = element_blank(),
+          axis.title = element_text(face="bold"),
+          axis.text = element_text(colour="black"),
+          legend.background = element_rect(colour="black"),
+          panel.grid.minor = element_blank()),
+  
+  ggplot() +
+    geom_bar(data=nitsum2022  %>% 
+               filter(is.na(Comments), Species=="Chum") %>%
+               group_by(Date) %>%
+               summarize(dead=sum(Dead,na.rm=T),Total=sum(Total,na.rm=T)) %>% 
+               mutate(ratioDL = ifelse(dead>0,dead/Total,0.0001)),
+             aes(x=as.Date(Date), y=ratioDL), 
+             stat="identity", width=1, fill="maroon") +
+    scale_x_date(date_breaks="1 day", date_labels="%b %d") +
+    labs(y="Ratio of live:dead Chum") +
+    theme_bw() +
+    theme(axis.text.x = element_text(angle=45, hjust=1),
+          axis.title.x = element_blank(),
+          axis.title = element_text(face="bold"),
+          axis.text = element_text(colour="black"),
+          legend.background = element_rect(colour="black"),
+          panel.grid.minor = element_blank()),
+  
+nrow=4
+)
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
